3ff83bbf87271f0f4bba9c42959ba58174e1de87
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:26:01 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@
             --with-browser=/usr/bin/elinks       \
             --with-vgrind=/usr/bin/vgrind        \
             --with-grap=/usr/bin/grap && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-sed -i "s:man man:root root:g" "${SHED_FAKEROOT}/usr/lib/tmpfiles.d/man-db.conf"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+sed -i "s:man man:root root:g" "${SHED_FAKE_ROOT}/usr/lib/tmpfiles.d/man-db.conf"
