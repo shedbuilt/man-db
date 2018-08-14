913182de317b53a1df5c654dca3b5b50b4487c10
Auston Stewart <auston@shedbuilt.net>
2018-08-14 12:37:21 -1000

    Updating to 2.8.4

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
-SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 # Configure
-./configure --prefix=/usr                        \
-            --docdir=$SHED_PKG_LOCAL_DOCDIR      \
-            --sysconfdir=/etc                    \
-            --disable-setuid                     \
-            --enable-cache-owner=bin             \
-            --with-browser=/usr/bin/elinks       \
-            --with-vgrind=/usr/bin/vgrind        \
+./configure --prefix=/usr                         \
+            --docdir="$SHED_PKG_DOCS_INSTALL_DIR" \
+            --sysconfdir=/etc                     \
+            --disable-setuid                      \
+            --enable-cache-owner=bin              \
+            --with-browser=/usr/bin/elinks        \
+            --with-vgrind=/usr/bin/vgrind         \
             --with-grap=/usr/bin/grap &&
 # Build and Install
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
 # Optionally Remove Documentation
 if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
-    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
 fi
